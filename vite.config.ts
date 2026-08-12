400c586f1304db026629bf03a8c219369bb7dce7
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-08-12 11:55:05 -0400

    Build sitemap automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { svelteSitemap } from 'svelte-sitemap';
+import { siteConfig } from './src/lib/config';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		svelteSitemap({
+			domain: siteConfig.url,
+		}),
+	],
 });
